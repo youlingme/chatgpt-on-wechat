f4be10041d3b470f980b3ec35446fb6316b129ec
wanghan <wanghan@heren.tech>
2023-06-27 17:17:44 +0800

    设置默认的chat类型为OPEN_AI 第三次

--- bridge/bridge.py
+++ bridge/bridge.py
@@ -31,7 +31,7 @@ class Bridge(object):
 
     def get_bot(self, typename):
         if self.bots.get(typename) is None:
-            logger.info("create bot {} for {}".format(self.btype[typename], typename))
+            logger.info("wanghan create bot {} for {}".format(self.btype[typename], typename))
             if typename == "text_to_voice":
                 self.bots[typename] = create_voice(self.btype[typename])
             elif typename == "voice_to_text":
